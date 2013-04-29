be8fa26e3a5e0760cae9cd281b991dc76044ff28
Shane M <shane@logo24.com>
2013-04-29 23:15:18 +0100

    Init commit
    Initial commit including wordpress files and updated css etc

---
+++ wordpress/header.php
@@ -0,0 +1,53 @@
+<!doctype html>
+
+<!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+
+	<head>
+		<meta charset="utf-8">
+
+		<!-- Google Chrome Frame for IE -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+		<title><?php wp_title(''); ?></title>
+
+		<meta name="msapplication-TileColor" content="#f01d4f">
+		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
+
+		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+
+		<!-- wordpress head functions -->
+		<?php wp_head(); ?>
+		<!-- end of wordpress head -->
+
+		<!-- drop Google Analytics Here -->
+		<!-- end analytics -->
+
+	</head>
+
+	<body <?php body_class(); ?>>
+
+		<div id="page_rap">
+			<div id="main_content">
+
+
+			<header id="content_holder" class="header" role="banner">
+
+
+  			<div id="top_img"> 
+
+	        <img src="/wordpress/wp-content/themes/eddiemachado-bones-6906c82/library/images/banner_logo.jpg" alt="Inhouse Services and Co logo"/>
+
+  			</div>
+
+				<div id="inner-header" class="wrap clearfix">
+
+					<div id="left"> 
+						<?php bones_main_nav(); ?>
+					</div>
+
+				</div> <!-- end #inner-header -->
+
+			</header> <!-- end header -->
